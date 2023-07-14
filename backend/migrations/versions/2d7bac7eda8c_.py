e61acb30e338cefe5bbcc3fbd3d0a8d3321e36d5
Vinh Pham <vinhpham@Vinhs-MacBook-Pro-2.local>
2023-07-13 20:04:58 -0700

    modify version of alembic

---
+++ backend/migrations/versions/2d7bac7eda8c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2d7bac7eda8c
+Revises: 7ab78285f571
+Create Date: 2023-07-13 20:03:54.292833
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2d7bac7eda8c'
+down_revision = '7ab78285f571'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
