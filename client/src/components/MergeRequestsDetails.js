224c1f0cacf27724b061f23015d718a92f89544c
Vinh Pham <vinhpham@Vinhs-MacBook-Pro-2.local>
2023-07-17 03:26:14 -0700

    tables

--- client/src/components/MergeRequestsDetails.js
+++ client/src/components/MergeRequestsDetails.js
@@ -11,30 +11,14 @@ const columns = [
   { field: "defect_severity", headerName: "Defect Severity", flex: 1 },
   { field: "detected_by", headerName: "Detected By", flex: 1 },
   {
-    field: "discussions",
+    field: "detail",
     headerName: "Discussion Details",
     flex: 4,
-    renderCell: (params) => {
-      return (
-        <div style={{ overflow: "auto" }}>
-          {params.value.map((discussion, index) => (
-            <p key={index}>
-              <span className="font-semibold">
-                [{discussion.defect_type_label}]
-              </span>
-              <span className="ml-1">[{discussion.defect_severity}]</span>
-              <span className="ml-1">{discussion.detail}</span>
-            </p>
-          ))}
-        </div>
-      );
-    },
   },
 ];
 
 function MergeRequestsDetails() {
   const [mergeRequests, setMergeRequests] = useState([]);
-  const [discussions, setDiscussions] = useState([]); // state variable to hold discussions
 
   useEffect(() => {
     axios
@@ -51,27 +35,10 @@ function MergeRequestsDetails() {
         rows={mergeRequests}
         columns={columns}
         rowsPerPageOptions={[5, 10, 20]}
-        getRowId={(row) => row.title}
+        getRowId={(row) => `${row.title}_${row.create_date}`}
         pagination
         rowHeight={100}
       />
-
-      <div className="mt-8">
-        {discussions.map((discussion, index) => (
-          <div
-            key={index}
-            className="mb-4 p-4 border rounded shadow-sm bg-white"
-          >
-            <span className="inline-block mr-2 px-2 py-1 text-xs font-semibold text-white bg-green-500 rounded">
-              {discussion.defect_type_label}
-            </span>
-            <span className="inline-block mr-2 px-2 py-1 text-xs font-semibold text-white bg-red-500 rounded">
-              {discussion.defect_severity}
-            </span>
-            <p className="mt-2 text-gray-700">{discussion.detail}</p>
-          </div>
-        ))}
-      </div>
     </div>
   );
 }
